ac70b29a46e778870da95e49141129995e4ccfb9
Sebastian Raskolnikow <s.raskolnikow@gmail.com>
2012-09-27 22:57:50 +0200

    pattern classes added

--- factory.cpp
+++ factory.cpp
@@ -1,5 +1,61 @@
 #include <string>
 
+class AbstractProductA
+{
+public:
+    
+};
+
+class ProduktA : public AbstractProductA
+{
+public:
+    
+private:
+    
+};
+
+
+class AbstractProductB
+{
+public:
+    
+};
+
+class ProductB : public AbstractProductB
+{
+public:
+    
+private:
+};
+
+class AbstractFactory
+{
+public:
+    virtual AbstractProduktA makeProduktA() {};
+    virtual AbstractProduktB makeProduktB() {};
+    
+protected:
+    
+private:
+    
+};
+
+class Factory : public AbstractFactory
+{
+public:
+    virtual AbstractProduktA makeProduktA() {
+        ProductA a;
+        return a;
+    };
+    
+    virtual AbstractProduktB makeProduktB() {
+        ProductB b;
+        return b;
+    };
+    
+private:
+};
+
 int main(void)
 {
     return 0;
